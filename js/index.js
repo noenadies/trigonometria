f55c08be29f06ff9519b2ed64b70242e0091e940
noenadies <33671023+noenadies@users.noreply.github.com>
2018-09-18 22:24:48 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,7 +11,10 @@ idbttrigonoetria.addEventListener('click', openquetrigonometri);
 closeBtn.addEventListener('click', closequetrigonometri);
 // Listen for outside click
 
+function habaandroid(sthabla){
+//window.Android.showToast("hablaandroid",sthabla,"");
 
+}
 // Function to open modal
 function openquetrigonometri(){
   modal.style.display = 'block';
@@ -26,9 +29,53 @@ function queestrigonometria(){
 	vquestrigonometr=document.getElementById("idpqueestrig").innerHTML;
 
 	//alert(vquestrigonometr);
-
-	window.Android.showToast("hablaandroid",vquestrigonometr,"");
+habaandroid(vquestrigonometr);
+//	window.Android.showToast("hablaandroid",vquestrigonometr,"");
 }
+
+
+
+
+
+
+
+// Get modal element
+var mdangulo = document.getElementById('midangulo');
+// Get open modal button
+var btangulo = document.getElementById('idbtangulo');
+// Get close button
+var btcloseangulo = document.getElementsByClassName('closeBtn')[1];
+
+// Listen for open click
+btangulo.addEventListener('click', openangulo);
+// Listen for close click
+closeBtn.addEventListener('click', closeangulo);
+// Listen for outside click
+
+
+// Function to open modal
+function openangulo(){
+  mdangulo.style.display = 'block';
+}
+
+// Function to close modal
+function closeangulo(){
+ mdangulo.style.display = 'none';
+}
+var vsangulo="";
+function hangulo(){
+	vsangulo=document.getElementById("idpangulo").innerHTML+document.getElementById("idpangulo2").innerHTML;
+
+	alert(vsangulo);
+habaandroid(vsangulo);
+	//window.Android.showToast("hablaandroid",vsangulo,"");
+}
+
+
+
+
+
+
 
 
 
@@ -41,9 +88,5 @@ function queestrigonometria(){
 fdefineangulo()
 
 function fdefineangulo(){
-	vquestrigonometr=document.getElementById("iddefineangulo").innerHTML;
-
-	alert(vquestrigonometr);
-
-	window.Android.showToast("hablaandroid",vquestrigonometr,"");
+	
 }
